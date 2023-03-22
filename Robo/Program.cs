5255c4f3589e2142df08976c9dc46c88501c96bb
FranciscoPaixao <26686029+FranciscoPaixao@users.noreply.github.com>
2023-03-22 14:21:21 -0300

    .

--- Robo/Program.cs
+++ Robo/Program.cs
@@ -15,27 +15,30 @@ namespace Robo
             int y = Convert.ToInt32(Convert.ToString(tamanho[0]));
             int x = Convert.ToInt32(Convert.ToString(tamanho[2]));
 
-            Console.WriteLine("Informe a posição inicial do robo 1");
-            String robo1 = Console.ReadLine();
-            int robo1x = Convert.ToInt32(Convert.ToString(robo1[0]));
-            int robo1y = Convert.ToInt32(Convert.ToString(robo1[2]));
-            String robo1d = robo1[4].ToString();
-            Console.WriteLine("Informe o comando a ser executado pelo robo 1");
-            String comandoR1 = Console.ReadLine();
+            int robo1x, robo1y, robo2x, robo2y;
+            string robo1d, comandoR1, robo2d, comandoR2;
 
-            Console.WriteLine("Informe a posição inicial do robo 2");
-            String robo2 = Console.ReadLine();
-            int robo2x = Convert.ToInt32(Convert.ToString(robo2[0]));
-            int robo2y = Convert.ToInt32(Convert.ToString(robo2[2]));
-            String robo2d = robo2[4].ToString();
-            Console.WriteLine("Informe o comando a ser executado pelo robo 2");
-            String comandoR2  = Console.ReadLine();
+            InputRobo(out robo1x, out robo1y, out robo1d, out comandoR1, 1);
+            InputRobo(out robo2x, out robo2y, out robo2d, out comandoR2, 2);
+
 
             Direcionamento(robo1x, robo1y, comandoR1, robo1d);
             Direcionamento(robo2x, robo2y, comandoR2, robo2d);
 
             while (true) ;
         }
+
+        private static void InputRobo(out int roboX, out int roboY, out string roboD, out string comando, int roboNumero)
+        {
+            Console.WriteLine($"Informe a posição inicial do robo {roboNumero}");
+            String robo1 = Console.ReadLine();
+            roboX = Convert.ToInt32(Convert.ToString(robo1[0]));
+            roboY = Convert.ToInt32(Convert.ToString(robo1[2]));
+            roboD = robo1[4].ToString();
+            Console.WriteLine($"Informe o comando a ser executado pelo robo {roboNumero}");
+            comando = Console.ReadLine();
+        }
+
         private static void Direcionamento(int x, int y, String comando, String direcao)
         {
             for (int i = 0; i < comando.Length; i++)
